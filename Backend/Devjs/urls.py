fb9c8a6a66d5981b0a213545828e1f766845ddd1
surin <tjdtnfls1128@gmail.com>
2025-04-04 08:53:47 +0900

    Modify Project name to Devjs

--- Backend/Devjs/urls.py
+++ Backend/Devjs/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for devjs project.
+URL configuration for Devjs project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
