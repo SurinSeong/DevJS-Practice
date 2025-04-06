9f25394952cafb5342f2a79d2a95e4151295b200
surin <tjdtnfls1128@gmail.com>
2025-04-06 18:49:04 +0900

    Delete urls

--- Backend/Devjs/urls.py
+++ Backend/Devjs/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),    # 로그인 관련
-    path('analyzes/', include('analyzes.urls')),    # 분석 관련
-    path('feedbacks/', include('feedbacks.urls')),    # 피드백 관련
+    
 ]
